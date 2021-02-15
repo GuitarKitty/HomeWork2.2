eb28402c2410e5ca26771606438c3d2a6905ff5d
Vyacheslav Efimenko <sakhguitarkitty@icloud.com>
2021-02-15 18:00:06 +1100

    deal it

---
+++ HomeWork2.2/ViewControllers/MainViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewController.swift
+//  HomeWork2.2
+//
+//  Created by Admin on 15.02.2021.
+//
+
+import UIKit
+
+protocol SettingsViewControllerDelegate {
+    func setColorMainView(
+        displayP3Red:CGFloat,
+        green:CGFloat,
+        blue:CGFloat,
+        alpha: Float
+)
+}
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var colorizedView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingVC = segue.destination as? SettingViewController else{ return }
+        if segue.identifier == "showSettingScreen"{
+            settingVC.delegate = self
+        }
+    }
+    
+    @IBAction func settingButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "showSettingScreen", sender: nil)
+    }
+
+    @IBAction func unwindSegueToLoginScreen(segue: UIStoryboardSegue) { }
+}
+
+extension MainViewController: SettingsViewControllerDelegate{
+    func setColorMainView(displayP3Red: CGFloat, green: CGFloat, blue: CGFloat, alpha: Float) {
+        colorizedView.backgroundColor = UIColor(displayP3Red: displayP3Red, green: green, blue: blue, alpha: CGFloat(alpha))
+    }
+}
--- HomeWork2.2/ViewControllers/SettingViewController.swift
+++ HomeWork2.2/ViewControllers/SettingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SettingViewController.swift
 //  HomeWork2.2
 //
 //  Created by Admin on 30.01.2021.
@@ -7,19 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class SettingViewController: UIViewController{
     
     @IBOutlet var showColor: UIView!
     @IBOutlet var redColorValue: UILabel!
     @IBOutlet var greenColorValue: UILabel!
     @IBOutlet var blueColorValue: UILabel!
-    
     @IBOutlet var sliderRedColor: UISlider!
     @IBOutlet var sliderGreenColor: UISlider!
     @IBOutlet var sliderBlueColor: UISlider!
+    var delegate: SettingsViewControllerDelegate!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-    private func changeShowColor(){
+        showColor.layer.cornerRadius = 20
+        slidersTintColor()
+        changeShowColor()
+        changeSliderValue()
+    }
+    
+    
+    @IBAction func colorChanger() {
+        changeSliderValue()
+        changeShowColor()
+    }
+    @IBAction func doneButtonPressed() {
+        let displayP3Red = CGFloat(sliderRedColor.value)
+        let green = CGFloat(sliderGreenColor.value)
+        let blue = CGFloat(sliderBlueColor.value)
+        let alpha = CGFloat(1.0)
+        if delegate != nil{
+        delegate.setColorMainView(
+            displayP3Red: displayP3Red,
+            green: green,
+            blue: blue,
+            alpha: Float(alpha))
+        dismiss(animated: true)
+        }
+        }
+    
+    private func slidersTintColor() {
+        sliderRedColor.tintColor = .red
+        sliderGreenColor.tintColor = .green
+        sliderBlueColor.tintColor = .blue
+    }
+    
+    private func changeShowColor() {
         showColor.backgroundColor = UIColor(
             displayP3Red: CGFloat(sliderRedColor.value),
             green: CGFloat(sliderGreenColor.value),
@@ -27,29 +61,14 @@ class ViewController: UIViewController {
             alpha: 1.0
         )
     }
- 
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        showColor.layer.cornerRadius = 20
-        changeShowColor()
-
+    private func changeSliderValue() {
         redColorValue.text = String(format: "%.2f", sliderRedColor.value)
         greenColorValue.text = String(format: "%.2f", sliderGreenColor.value)
         blueColorValue.text = String(format: "%.2f", sliderBlueColor.value)
     }
-    
-    @IBAction func redColorChanger() {
-        redColorValue.text = String(format: "%.2f", sliderRedColor.value)
-        greenColorValue.text = String(format: "%.2f", sliderGreenColor.value)
-        blueColorValue.text = String(format: "%.2f", sliderBlueColor.value)
-        changeShowColor()
-    }
 
 }
 
-func slidersTintColor(){
 
-}
 
